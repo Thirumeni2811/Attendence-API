5f42a4e60a623ecf83988fa965d036257d61c2c0
Thirumeni2811 <thirumeni948@gmail.com>
2025-03-22 16:08:14 +0530

    Creator Role added

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,9 +1,10 @@
-const roles = ["admin", "subAdmin", "user"];
+const roles = ["creator", "admin", "subAdmin", "user"];
 const roleRights = new Map();
 
-roleRights.set(roles[0], ["getUsers", "manageUsers"]); // admin
-roleRights.set(roles[1], ["getUsers", "manageUsers"]); // subAdmin
-roleRights.set(roles[2], []); // user (basic access)
+roleRights.set(roles[0], ["getAll", "getUsers", "manageUsers"]); // admin
+roleRights.set(roles[1], ["getUsers", "manageUsers"]); // admin
+roleRights.set(roles[2], ["getUsers", "manageUsers"]); // subAdmin
+roleRights.set(roles[3], []); // user (basic access)
 
 module.exports = {
   roles,
